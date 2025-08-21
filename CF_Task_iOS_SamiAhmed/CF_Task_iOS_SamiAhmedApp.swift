95ee4407b86b1b3e64ffaaf8b78e56c281bb17d3
Sami Ahmed <samiiiahmed8@gmail.com>
2025-08-21 15:36:20 +0300

    Initial Commit

---
+++ CF_Task_iOS_SamiAhmed/CF_Task_iOS_SamiAhmedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CF_Task_iOS_SamiAhmedApp.swift
+//  CF_Task_iOS_SamiAhmed
+//
+//  Created by Sami on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CF_Task_iOS_SamiAhmedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
